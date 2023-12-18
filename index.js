0496287c7a47b3f07cdf217ad3882a14f5f6ecea
Rene <racosta323@gmail.com>
2023-12-18 13:56:04 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+
+function writeCards(names){
+    let birthday = [];
+    for(let i = 0; i < names.length; i++){
+    birthday.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+    }
+    return birthday;
+}
+
+writeCards(names);
+
+
+
+function countDown(number){
+    while (number >= 0){
+         console.log(number);
+         number--;
+       
+    }
+}
+
+countdown(10);
+
+
